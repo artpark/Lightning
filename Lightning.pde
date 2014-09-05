00b64696ab98201152726a8a032da012109f3c3c
artpark <art.parkeenvincha@gmail.com>
2014-09-05 09:58:04 -0700

    Added first name to index.html

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,47 @@
+//Art Parkeenvincha, Block 3, Lightning
+float startX = 0;
+float startY = 300;
+float endX = 0;
+float endY = 300;
+float weight = 10;
+
 void setup()
 {
-  size(300,300);
+  size(600,600);
+  colorMode(HSB);
+  strokeWeight(0.1);
+  background(110);
+  stroke(255);
 }
 void draw()
 {
-
+  background(110);
+  while(endX < width){
+    for (int i = 0; i < 2; i++){
+      
+      endX = startX + random(0, 9);
+      endY = startY + random(-9, 9);    
+  
+//      stroke(#036DFF);
+      strokeWeight(weight);  
+      line(startX, startY, endX, endY);
+//      stroke(0,0,255);
+      strokeWeight(weight - 0.50);  
+      line(startX, startY, endX, endY);      
+  
+      startX = endX;
+      startY = endY;
+      
+      weight -= 0.06;
+    }
+  }
+  
+//  if(keyPressed == true && key == ' ')
+//  {
+    startX = 0;
+    startY = 300;
+    endX = 0;
+    endY = 300;
+    weight = 10;
+//  }
 }
-void mousePressed()
-{
-
-}
-
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,21 +14,53 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+//Art Parkeenvincha, Block 3, Lightning
+float startX = 0;
+float startY = 300;
+float endX = 0;
+float endY = 300;
+float weight = 10;
+
 public void setup()
 {
-  size(300,300);
-  noLoop();
+  size(600,600);
+  colorMode(HSB);
+  strokeWeight(0.1f);
+  background(110);
+  stroke(255);
 }
 public void draw()
 {
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+  background(110);
+  while(endX < width){
+    for (int i = 0; i < 2; i++){
+      
+      endX = startX + random(0, 9);
+      endY = startY + random(-9, 9);    
+  
+//      stroke(#036DFF);
+      strokeWeight(weight);  
+      line(startX, startY, endX, endY);
+//      stroke(0,0,255);
+      strokeWeight(weight - 0.50f);  
+      line(startX, startY, endX, endY);      
+  
+      startX = endX;
+      startY = endY;
+      
+      weight -= 0.06f;
+    }
+  }
+  
+//  if(keyPressed == true && key == ' ')
+//  {
+    startX = 0;
+    startY = 300;
+    endX = 0;
+    endY = 300;
+    weight = 10;
+//  }
 }
-public void mousePressed()
-{
-  redraw();
-}
-
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Lightning" };
     if (passedArgs != null) {
